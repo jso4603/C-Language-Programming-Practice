d0172e8598d174c25f4d923ca4ceabfd6ccedbfd
layboy <jso46033@naver.com>
2018-11-18 23:52:42 +0900

    4-1 add

---
+++ 4-1/4-1/4-1/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  4-1
+//
+//  Created by 장상옥 on 2018. 11. 18..
+//  Copyright © 2018년 장상옥. All rights reserved.
+//
+
+#include<stdio.h>
+
+int main(void)
+{
+    printf("Hello world!");
+    
+    return 0;
+}
